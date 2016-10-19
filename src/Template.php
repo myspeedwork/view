43082377a943fb55c220bb255f20d1ead1be2125
sankar <sankar.suda@gmail.com>
2016-10-19 16:03:31 +0530

    cleanup and refracted

--- src/Template.php
+++ src/Template.php
@@ -12,14 +12,14 @@
 namespace Speedwork\View;
 
 use Speedwork\Core\Di;
-use Speedwork\Core\Traits\RequestTrait;
+use Speedwork\Core\Traits\HttpTrait;
 
 /**
  * @author sankar <sankar.suda@gmail.com>
  */
 class Template extends Di
 {
-    use RequestTrait;
+    use HttpTrait;
 
     protected $breadcrumbs = [];
 
@@ -28,14 +28,14 @@ class Template extends Di
      *
      * @var string
      */
-    protected $_base = '';
+    protected $baseLink = '';
 
     /**
      * Document base URL.
      *
      * @var string
      */
-    protected $_basetarget = '_self';
+    protected $baseTarget = '_self';
 
     /**
      * Array of meta tags.
@@ -136,14 +136,44 @@ class Template extends Di
     }
 
     /**
+     * Set the page title.
+     *
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        return $this->setMeta('title', $title);
+    }
+
+    /**
+     * Set the page description.
+     *
+     * @param string $description
+     */
+    public function setDescn($description)
+    {
+        return $this->setMeta('description', $description);
+    }
+
+    /**
+     * Set the page keywords.
+     *
+     * @param string $keywords
+     */
+    public function setKeywords($keywords)
+    {
+        return $this->setMeta('keywords', $keywords);
+    }
+
+    /**
      * Sets the base URI of the document.
      *
      * @param string $base
      */
     public function setBase($base, $target = '_self')
     {
-        $this->_base       = $base;
-        $this->_basetarget = $target;
+        $this->baseLink   = $base;
+        $this->baseTarget = $target;
 
         return $this;
     }
@@ -155,7 +185,7 @@ class Template extends Di
      */
     public function getBase()
     {
-        return $this->_base;
+        return $this->baseLink;
     }
 
     public function setDefaults()
@@ -206,7 +236,7 @@ class Template extends Di
         if ($this->getBase()) {
             $html .= '<base ';
             $html .= ($this->getBase()) ? 'href="'.$this->getBase().'" ' : ' ';
-            $html .= 'target="'.$this->_basetarget.'"'." />\n";
+            $html .= 'target="'.$this->baseTarget.'"'." />\n";
         }
 
         $html .= $this->generateMeta();
@@ -261,11 +291,11 @@ class Template extends Di
      */
     private function fetchTemplate($file = '')
     {
-        $layout = config('view.theme.layout');
+        $layout = $this->config('view.theme.layout');
         $layout = $layout ?: 'index';
         $file   = $file ?: $layout;
 
-        $extensions = config('view.extensions');
+        $extensions = $this->config('view.extensions');
         $extensions = $extensions ?: ['tpl'];
 
         $files = [
@@ -291,56 +321,71 @@ class Template extends Di
         return '';
     }
 
-    public function render()
-    {
-        return $this->onBeforeRenderTemplate();
-    }
-
-    protected function onBeforeRenderTemplate()
+    /**
+     * Identify the request type.
+     */
+    protected function checkRequestType()
     {
         //check that is ajax request
-        if ($this->data['_request'] == 'iframe'
-            || $this->data['_request'] == 'ajax'
+        if ($this->input('_request') == 'iframe'
+            || $this->input('_request') == 'ajax'
             || strtolower(env('HTTP_X_REQUESTED_WITH')) == 'xmlhttprequest'
-            || ($this->type) || ($this->layout) || ($this->format)
+            || ($this->get('type')) || ($this->get('layout')) || ($this->get('format'))
         ) {
             $this->sets('is_ajax_request', true);
         }
 
-        if ($this->data['_request'] == 'iframe') {
+        if ($this->input('_request') == 'iframe') {
             $this->sets('is_iframe_request', true);
         }
+    }
 
+    /**
+     * Check is application offline.
+     *
+     * @return bool
+     */
+    protected function isOffline()
+    {
         //check whether this site is in offline
         if ($this->config('app.offline.enable')) {
-            if ($this->data['allowme']) {
-                $this->get('session')->set('allowme', $this->data['allowme']);
+            if ($this->input('allowme')) {
+                $this->get('session')->set('allowme', $this->input('allowme'));
             }
 
             $allow = $this->get('session')->get('allowme');
             $key   = $this->config('app.offline.key');
 
-            if ((empty($allow) || $allow != $key)) {
-                return $this->fetchTemplate('offline');
+            if ($allow && $allow === $key) {
+                return false;
             }
+
+            return true;
         }
 
-        $is_logged_in    = $this->get('is_user_logged_in');
-        $is_ajax_request = $this->get('is_ajax_request');
-        $rule            = $this->get('rule');
+        return false;
+    }
 
-        // default allow to every one
-        $allowed = $this->get('acl')->isGranted($rule);
-        $next    = $is_ajax_request ? '' : $this->get('request')->fullUrlWithQuery([]);
-        $next    = $this->data['next'] ?: $next;
+    /**
+     * Check is allowed to view the requested page.
+     *
+     * @return string|array
+     */
+    protected function isNotGranted()
+    {
+        $is_logged_in = $this->get('is_user_logged_in');
+        $isAjax       = $this->get('is_ajax_request');
 
-        if (!$allowed && $is_ajax_request) {
+        $next = $isAjax ? '' : $this->get('request')->fullUrlWithQuery([]);
+        $next = $this->input('next') ?: $next;
+
+        if ($isAjax) {
             if ($this->type == 'html' || $this->format == 'html') {
                 if (!$is_logged_in) {
-                    return  '<div class="alert alert-info text-bold">Please <a data-next="'.$next.'" href="'.$this->link('members/login?next='.urldecode($next)).'" role="login">login</a> to your account.</div>';
+                    return '<div class="alert alert-info text-bold">Please <a data-next="'.$next.'" href="'.$this->link('members/login?next='.urldecode($next)).'" role="login">login</a> to your account.</div>';
                 }
 
-                return  '<div class="alert alert-danger text-bold">Your don\'t have sufficient permissions.</div>';
+                return '<div class="alert alert-danger text-bold">'.trans('Your don\'t have sufficient permissions.').'</div>';
             }
 
             $status           = [];
@@ -348,16 +393,16 @@ class Template extends Di
             if (!$is_logged_in) {
                 $status['login']   = true;
                 $status['next']    = $next;
-                $status['message'] = 'Please login to your account.';
+                $status['message'] = trans('Please login to your account.');
             } else {
-                $status['message'] = 'Your don\'t have sufficient permissions..';
+                $status['message'] = trans('Your don\'t have sufficient permissions.');
             }
 
             return $status;
         }
 
         //for gusets
-        if (!$allowed && !$is_logged_in) {
+        if (!$is_logged_in) {
             $link = $this->config('auth.account.guest');
             if (empty($link)) {
                 $link = 'members/login';
@@ -367,65 +412,88 @@ class Template extends Di
             return $this->redirect($link);
         }
 
-        //for already loggedin users
-        if (!$allowed && $is_logged_in) {
-            return $this->redirect('errors/denied');
+        return $this->redirect('errors/denied');
+    }
+
+    /**
+     * Render the request.
+     *
+     * @return string|array
+     */
+    public function render()
+    {
+        if ($this->isOffline()) {
+            return $this->fetchTemplate('offline');
+        }
+
+        $this->checkRequestType();
+
+        $rule      = $this->get('rule');
+        $isGranted = $this->get('acl')->isGranted($rule);
+        $isAjax    = $this->get('is_ajax_request');
+
+        if (!$isGranted) {
+            return $this->isNotGranted();
         }
 
         //check that is ajax request
-        if ($is_ajax_request) {
+        if ($isAjax) {
             return $this->renderAjax();
         }
 
         return $this->fetchTemplate();
     }
 
-    public function renderAjax()
+    /**
+     * Render the ajax request.
+     *
+     * @return string|array
+     */
+    protected function renderAjax()
     {
         $formats = ['raw', 'xml', 'rss', 'json', 'js', 'jsonp', 'script'];
         $route   = $this->get('route');
+        $format  = $this->get('format');
+        $type    = $this->get('type');
+        $layout  = $this->get('layout');
 
-        if ($this->format == 'js' || $this->format == 'script') {
-            header('Content-Type: application/javascript');
+        if ($format == 'js' || $format == 'script') {
+            $this->setHeader('Content-Type', 'application/javascript');
         }
 
-        if ($this->format == 'xml') {
-            header('Content-Type: text/xml');
+        if ($format == 'xml') {
+            $this->setHeader('Content-Type', 'text/xml');
         }
 
-        if ($this->type == 'module' && empty($this->format)) {
+        if ($type == 'module' && empty($format)) {
             return $this->get('resolver')->module($route);
         }
 
-        if ($this->type == 'widget') {
+        if ($type == 'widget') {
             return $this->get('resolver')->widget($route, [], true);
         }
 
-        if ($this->type == 'html' || $this->format == 'html') {
+        if ($type == 'html' || $format == 'html') {
             $file = 'html';
 
             return $this->fetchTemplate($file);
         }
 
-        if (($this->type) || ($this->format) || ($this->layout)) {
-            if ((!$this->format)) {
-                $file = ($this->layout) ? str_replace('..', '', $this->layout) : 'component';
+        if ($type || $format || $layout) {
+            if ((!$format)) {
+                $file = ($layout) ? str_replace('..', '', $layout) : 'component';
 
                 return $this->fetchTemplate($file);
             }
 
-            if (in_array($this->type, $formats) || in_array($this->format, $formats)) {
-                if ($this->type == 'module') {
+            if (in_array($type, $formats) || in_array($format, $formats)) {
+                if ($type == 'module') {
                     $response = $this->get('resolver')->loadModuleController($route);
                 } else {
                     $response = $this->get('resolver')->loadController($route);
                 }
 
-                if ($this->format == 'json' || $this->format == 'jsonp') {
-                    if (!is_array($response)) {
-                        $response = $this->release('status');
-                    }
-
+                if ($format == 'json' || $format == 'jsonp') {
                     if (!is_array($response)) {
                         $response = [];
                     }
@@ -528,11 +596,26 @@ class Template extends Di
         return $this->$func($name, $attribs);
     }
 
-    protected function renderModule($name, $attribs)
+    /**
+     * Render module.
+     *
+     * @param string $name    name of the module
+     * @param array  $attribs Extra attributes
+     *
+     * @return string
+     */
+    protected function renderModule($name, array $attribs = [])
     {
         return $this->get('resolver')->module($name, $attribs);
     }
 
+    /**
+     * Render modules in position.
+     *
+     * @param string $position
+     *
+     * @return string
+     */
     protected function renderModules($position)
     {
         if (empty($position)) {
@@ -542,13 +625,21 @@ class Template extends Di
         return $this->get('resolver')->modules($position);
     }
 
-    protected function renderPosition($position, $attribs = [])
+    /**
+     * Render modules and components in position.
+     *
+     * @param string $position
+     * @param array  $attribs
+     *
+     * @return string
+     */
+    protected function renderPosition($position, array $attribs = [])
     {
         if (empty($position)) {
             return;
         }
 
-        $modules = config('view.modules');
+        $modules = $this->config('view.modules');
         if (!is_array($modules) || !isset($modules[$position])) {
             return;
         }
@@ -568,18 +659,36 @@ class Template extends Di
         return $content;
     }
 
-    protected function renderComponent($name, $attribs)
+    /**
+     * Render component.
+     *
+     * @param string|null $name    name of the route
+     * @param array       $attribs Any extra attribs
+     *
+     * @return string
+     */
+    protected function renderComponent($name, array $attribs = [])
     {
-        $name = $name ?: $this->route;
+        $name = $name ?: $this->get('route');
 
         return $this->get('resolver')->component($name, $attribs);
     }
 
+    /**
+     * Render the header position.
+     *
+     * @return string
+     */
     protected function renderHeader()
     {
         return $this->fetchHead();
     }
 
+    /**
+     * Render footer position.
+     *
+     * @return string
+     */
     protected function renderFooter()
     {
         return $this->fetchFooter();
